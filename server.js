3839386d08e3b9328cf64883bfbb3583dc2d64b9
Millenniumist <suparit.panta@gmail.com>
2024-10-28 14:46:47 +0700

    change port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 9999;
+const port = process.env.PORT || 9998;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
